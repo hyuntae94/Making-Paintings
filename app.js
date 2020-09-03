9898172979b61370a17508b6000f2e4b42afb866
hyuntae94 <believe4279@gmail.com>
2020-09-03 15:38:30 +0900

    Brush Size

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 const canvas = document.getElementById("jsCanvas");
 const ctx = canvas.getContext("2d");
 const colors = document.getElementsByClassName("jsColor");
+const range = document.getElementById("jsRange");
+const mode = document.getElementById("jsMode");
 
 canvas.width = 700;
 canvas.height = 700;
@@ -9,6 +11,7 @@ ctx.lineWidth = 5;
 ctx.strokeStyle = "#2c2c2c";
 
 let painting = false;
+let filling = false;
 
 function stopPainting() {
 	painting = false;
@@ -28,11 +31,22 @@ function canvasMonitoring(event) {
 		ctx.lineTo(x, y);
 		ctx.stroke();
 	}
-
 }
 
+const handleRange = (event) => {
+	const size = event.target.value;
+	ctx.lineWidth = size;
+}
+const handleModeClick = () => {
+	if (filling === true) {
+		mode.innerText = "Fill";
+		filling = false;
+	} else {
+		filling = true;
+		mode.innerText = "Paint";
+	}
+}
 const handleColorClick = (event) => ctx.strokeStyle = event.target.style.backgroundColor;
-
 
 function init() {
 	canvas.addEventListener("mousemove", canvasMonitoring);
@@ -46,4 +60,11 @@ if (canvas) {
 	init();
 }
 
+if (range) {
+	range.addEventListener("input", handleRange);
+}
+
+if (mode) {
+	mode.addEventListener("click", handleModeClick);
+}
 Array.from(colors).forEach(color => color.addEventListener("click", handleColorClick));
